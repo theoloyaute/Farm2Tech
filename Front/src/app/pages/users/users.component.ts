f7607f67e142ee5d94b0cfb01abc1540a6e072bb
theoloyaute <theo.loyaute05@gmail.com>
2023-04-19 15:58:51 +0200

    Adding route users

---
+++ Front/src/app/pages/users/users.component.ts
@@ -0,0 +1,22 @@
+import {Component, OnInit} from '@angular/core';
+import {User} from "../../models/users";
+import {UsersService} from "../../services/users.service";
+
+@Component({
+  selector: 'app-users',
+  templateUrl: './users.component.html',
+  styleUrls: ['./users.component.css']
+})
+export class UsersComponent implements OnInit {
+
+  users: User[] = [];
+    constructor(private UsersService: UsersService) { }
+
+  ngOnInit(): void {
+    this.UsersService.getUsers().subscribe((users: User[]) => {
+      this.users = users;
+    });
+  }
+
+
+}
--- Front/src/app/services/users.service.ts
+++ Front/src/app/services/users.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import {Router} from "@angular/router";
+import {HttpClient, HttpParams} from "@angular/common/http";
+import {Observable} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsersService {
 
-  constructor() { }
+  protected baseUrl = 'http://localhost:5276/api';
+  protected componentUrl = this.baseUrl + '/Users';
+
+  constructor(
+    protected router: Router,
+    protected http: HttpClient
+  ) { }
+
+  getUsers(): Observable<any>{
+    const params: HttpParams = new HttpParams();
+    return this.http.get(this.componentUrl, {params})
+  }
 }
